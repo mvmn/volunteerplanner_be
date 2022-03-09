10a901dcd7e8e7fd2b538929b18cd3a0acfeb502
Mykola Makhin <mvmn.inbox@gmail.com>
2022-03-09 23:47:39 +0200

    Revert "Added MapStruct"
    This reverts commit 0189b2efccc266c28e2bd0917aff98f36449ff8e.

--- manager-api/src/main/java/com/volunteer/api/data/user/UserControllerV1.java
+++ manager-api/src/main/java/com/volunteer/api/data/user/UserControllerV1.java
@@ -1,7 +1,11 @@
 package com.volunteer.api.data.user;
 
+import com.volunteer.api.data.user.mapping.UserV1Mapper;
+import com.volunteer.api.data.user.model.api.UserV1;
+import com.volunteer.api.data.user.model.dto.UserDto;
+import com.volunteer.api.data.user.service.UserService;
 import java.util.Collection;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,31 +18,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.volunteer.api.data.user.mapping.UserV1Mapper;
-import com.volunteer.api.data.user.model.api.UserV1;
-import com.volunteer.api.data.user.model.dto.UserDto;
-import com.volunteer.api.data.user.service.UserService;
-
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
-@RequiredArgsConstructor
 public class UserControllerV1 {
 
   private final UserService service;
-  private final UserV1Mapper userV1Mapper;
+
+  public UserControllerV1(@Autowired final UserService service) {
+    this.service = service;
+  }
 
   @PreAuthorize("hasAuthority('ROLE_ADMIN')")
   @GetMapping
   public ResponseEntity<Collection<UserV1>> getAll() {
-    final Collection<UserV1> result = userV1Mapper.map(service.getAll());
+    final Collection<UserV1> result = UserV1Mapper.map(service.getAll());
     return ResponseEntity.ok(result);
   }
 
   @GetMapping(path = "/{user-id}")
   public ResponseEntity<UserV1> getById(@PathVariable("user-id") final Integer userId) {
-    final UserV1 result = userV1Mapper.map(service.get(userId)
+    final UserV1 result = UserV1Mapper.map(service.get(userId)
         .orElseThrow(() -> new IllegalArgumentException(String.format(
             "User with id '%d' does not exist", userId))));
 
@@ -47,17 +46,17 @@ public class UserControllerV1 {
 
   @PostMapping
   public ResponseEntity<UserV1> create(@RequestBody final UserV1 source) {
-    final UserV1 result = userV1Mapper.map(service.create(userV1Mapper.map(source)));
+    final UserV1 result = UserV1Mapper.map(service.create(UserV1Mapper.map(source)));
     return ResponseEntity.status(HttpStatus.CREATED).body(result);
   }
 
   @PutMapping(path = "/{user-id}")
   public ResponseEntity<UserV1> update(@PathVariable("user-id") final Integer userId,
       @RequestBody final UserV1 source) {
-    final UserDto userDto = userV1Mapper.map(source);
+    final UserDto userDto = UserV1Mapper.map(source);
     source.setId(userId);
 
-    final UserV1 result = userV1Mapper.map(service.create(userDto));
+    final UserV1 result = UserV1Mapper.map(service.create(userDto));
     return ResponseEntity.status(HttpStatus.OK).body(result);
   }
 
