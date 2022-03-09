eb7deae4b67e9e1a6d3915a44fede305dc933b2c
Mykola Makhin <mvmn.inbox@gmail.com>
2022-03-09 22:38:13 +0200

    Added MapStruct

--- manager-api/src/main/java/com/volunteer/api/data/user/mapping/UserV1Mapper.java
+++ manager-api/src/main/java/com/volunteer/api/data/user/mapping/UserV1Mapper.java
@@ -1,17 +1,19 @@
 package com.volunteer.api.data.user.mapping;
 
 import java.util.Collection;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import com.volunteer.api.data.user.model.api.UserV1;
 import com.volunteer.api.data.user.model.dto.RoleDto;
 import com.volunteer.api.data.user.model.dto.UserDto;
 
+@Mapper(componentModel = "spring")
 public abstract class UserV1Mapper {
 
   @Mapping(target = "role", source = "role.name")
-  @Mapping(target = "password", constant = "******")
   public abstract UserV1 map(UserDto dto);
 
+  @Mapping(target = "password", constant = "******")
   public abstract UserDto map(UserV1 user);
 
   public RoleDto map(String roleName) {
