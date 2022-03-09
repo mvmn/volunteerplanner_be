cc4da12e34435d5cb558150d20ad9394e92b3b68
Sergii Sergeiev <SergSergeyev@gmail.com>
2022-03-09 16:12:09 +0200

    VOL-1 Address management

---
+++ manager-api/src/main/java/com/volunteer/api/data/user/AddressController.java
@@ -0,0 +1,60 @@
+package com.volunteer.api.data.user;
+
+import com.volunteer.api.data.user.mapping.AddressDtoMapper;
+import com.volunteer.api.data.user.model.api.AddressDtoV1;
+import com.volunteer.api.data.user.model.dto.Address;
+import com.volunteer.api.data.user.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/address", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @Autowired
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping(path = "/{address-id}")
+    public ResponseEntity<AddressDtoV1> getById(@PathVariable("address-id") final Integer addressId) {
+        Optional<Address> address = addressService.get(addressId);
+        return address
+                .map(a -> ResponseEntity.ok(AddressDtoMapper.map(a)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressDtoV1> create(@RequestBody final AddressDtoV1 address) {
+        AddressDtoV1 result = AddressDtoMapper.map(addressService.save(AddressDtoMapper.map(address)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping(path = "/regions")
+    public ResponseEntity<List<String>> getRegions() {
+        return ResponseEntity.ok(addressService.getDictinctRegions(null));
+    }
+
+    @GetMapping(path = "/regions/{region}")
+    public ResponseEntity<List<String>> getRegions(@PathVariable("region") final String region) {
+        return ResponseEntity.ok(addressService.getDictinctRegions(region));
+    }
+
+    @GetMapping(path = "/cities")
+    public ResponseEntity<List<String>> getAllCities() {
+        return ResponseEntity.ok(addressService.getDictinctCities(null));
+    }
+
+    @GetMapping(path = "/cities/{city}")
+    public ResponseEntity<List<String>> getCities(@PathVariable("city") final String city) {
+        return ResponseEntity.ok(addressService.getDictinctCities(city));
+    }
+}
---
+++ manager-api/src/main/java/com/volunteer/api/data/user/service/impl/AddressServiceImpl.java
@@ -0,0 +1,50 @@
+package com.volunteer.api.data.user.service.impl;
+
+import com.volunteer.api.data.user.model.dto.Address;
+import com.volunteer.api.data.user.repository.AddressRepository;
+import com.volunteer.api.data.user.service.AddressService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressRepository addressRepository;
+
+    @Autowired
+    public AddressServiceImpl(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public Collection<Address> getAll() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public Optional<Address> get(Integer id) {
+        return addressRepository.findById(id);
+    }
+
+    @Override
+    public Address save(Address address) {
+        Optional<Address> existing = addressRepository.findByRegionAndCityAndAddress(
+                address.getRegion(), address.getCity(), address.getAddress());
+        return existing.orElse(addressRepository.save(address));
+    }
+
+    @Override
+    public List<String> getDictinctRegions(String region) {
+        return addressRepository.findDistinctRegions(StringUtils.defaultIfBlank(region, ""));
+    }
+
+    @Override
+    public List<String> getDictinctCities(String city) {
+        return addressRepository.findDistinctCities(StringUtils.defaultIfBlank(city, ""));
+    }
+}
