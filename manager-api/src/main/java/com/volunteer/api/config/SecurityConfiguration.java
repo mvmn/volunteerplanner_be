e8a179fef5cbed322dc1a60b52c14121f4beded5
Mykola Makhin <mvmn.inbox@gmail.com>
2022-03-10 11:41:32 +0200

    Only allow OpenAPI docs and SwaggerUI if profile is local

--- manager-api/src/main/java/com/volunteer/api/config/SecurityConfiguration.java
+++ manager-api/src/main/java/com/volunteer/api/config/SecurityConfiguration.java
@@ -1,14 +1,19 @@
 package com.volunteer.api.config;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,43 +31,55 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private PasswordEncoder passwordEncoder;
-  @Autowired
-  private UserDetailsService userService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	@Autowired
+	private UserDetailsService userService;
+	@Autowired
+	private Environment environment;
 
-  @Override
-  protected void configure(final AuthenticationManagerBuilder builder) throws Exception {
-    builder.userDetailsService(userService).passwordEncoder(passwordEncoder);
-  }
+	@Override
+	protected void configure(final AuthenticationManagerBuilder builder) throws Exception {
+		builder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+	}
 
-  @Override
-  protected void configure(final HttpSecurity http) throws Exception {
-    http.csrf().disable();
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    http.authorizeRequests()
-        .antMatchers("/actuator/**").permitAll()
-        .antMatchers("/authenticate").permitAll()
-        .antMatchers("/swagger-ui/**").permitAll()
-        .antMatchers("/swagger-ui.html").permitAll()
-        .antMatchers("/v3/**").permitAll()
-        .anyRequest().authenticated();
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		configureOpenApi(http.authorizeRequests().antMatchers("/actuator/**").permitAll().antMatchers("/authenticate").permitAll())
+				.anyRequest()
+				.authenticated();
 
-    http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-  }
-
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
+		http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
 
 	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	@Profile("local")
 	public OpenAPI customOpenAPI() {
 
 		final String securitySchemeName = "bearerAuth";
 		return new OpenAPI().addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
 				.components(new Components().addSecuritySchemes(securitySchemeName,
 						new SecurityScheme().name(securitySchemeName).type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+	}
+
+	private ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry configureOpenApi(
+			ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry urlRegistry) {
+		if (Set.of(environment.getActiveProfiles()).contains("local")) {
+			return urlRegistry.antMatchers("/swagger-ui/**")
+					.permitAll()
+					.antMatchers("/swagger-ui.html")
+					.permitAll()
+					.antMatchers("/v3/**")
+					.permitAll();
+		}
+		return urlRegistry;
 	}
 }
