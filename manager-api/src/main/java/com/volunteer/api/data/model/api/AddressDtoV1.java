56db53d70e3d8d89f200ec4050844b8abe20556d
Mykola Makhin <mvmn.inbox@gmail.com>
2022-03-16 00:22:00 +0200

    VOL-40: Bring back no-args constructor to address DTO v1

--- manager-api/src/main/java/com/volunteer/api/data/model/api/AddressDtoV1.java
+++ manager-api/src/main/java/com/volunteer/api/data/model/api/AddressDtoV1.java
@@ -2,11 +2,15 @@ package com.volunteer.api.data.model.api;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonPropertyOrder({"id", "region", "city", "address"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressDtoV1 {
