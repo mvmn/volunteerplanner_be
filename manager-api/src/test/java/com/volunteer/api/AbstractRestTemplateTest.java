2f36e79fe8a203b7d4648207b7311baa6f3b38f3
Maksym Molodukhy-Lozunskyy <Maksym_Molodukhy-Lozunskyy@epam.com>
2022-03-25 12:45:58 +0200

    unit tests

--- manager-api/src/test/java/com/volunteer/api/AbstractRestTemplateTest.java
+++ manager-api/src/test/java/com/volunteer/api/AbstractRestTemplateTest.java
@@ -3,18 +3,17 @@ package com.volunteer.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.volunteer.api.utils.JsonTestUtils;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -22,29 +21,18 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.volunteer.api.utils.JsonTestUtils;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@Testcontainers
-@ActiveProfiles("test")
-public class UserManagementTest extends AbstractTestWithPersistence {
+public class AbstractRestTemplateTest extends AbstractTestWithPersistence {
 
   private static final ParameterizedTypeReference<JsonNode> JSON_NODE_TYPE_REFERENCE =
       new ParameterizedTypeReference<>() {
       };
 
-  private static final String TEST_CASES_FILE_PATH = "data/user-mgmt-e2e-tests.json";
-
   @Autowired
-  private TestRestTemplate restTemplate;
+  protected TestRestTemplate restTemplate;
 
-  @ParameterizedTest()
-  @MethodSource("loadClassPassTestCases")
-  public void endpointTest(final String testName, final JsonNode given, final JsonNode expected) {
+  public void endpointTest(final String testName, final JsonNode given, final JsonNode expected,
+      final Set<String> assertIgnoreProperties) {
     final ResponseEntity<JsonNode> actual = executeRequest(given);
 
     assertEquals(expected.get("status").asInt(), actual.getStatusCodeValue(),
@@ -54,8 +42,8 @@ public class UserManagementTest extends AbstractTestWithPersistence {
     if (JsonTestUtils.isNull(expectedBody)) {
       assertNull(actual.getBody(), String.format("[%s] response body", testName));
     } else {
-      Assertions.assertTrue(JsonTestUtils.equals(expectedBody, actual.getBody(),
-              Set.of("userVerifiedAt", "lockedAt")),
+      Assertions.assertTrue(JsonTestUtils.equals(
+              expectedBody, actual.getBody(), assertIgnoreProperties),
           String.format("[%s] response body. \nExpected: '%s' \nActual:   '%s'", testName,
               JsonTestUtils.toJsonString(expectedBody),
               JsonTestUtils.toJsonString(actual.getBody())));
@@ -93,16 +81,16 @@ public class UserManagementTest extends AbstractTestWithPersistence {
     final HttpHeaders headers = new HttpHeaders();
     token.ifPresent(value -> headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + value));
 
-    if (Objects.isNull(body) || body.isMissingNode()) {
+    if (JsonTestUtils.isNull(body)) {
       return new HttpEntity<>(null, headers);
-    } else {
-      headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
-      return new HttpEntity<>(body, headers);
     }
+
+    headers.add(HttpHeaders.CONTENT_TYPE, "application/json");
+    return new HttpEntity<>(body, headers);
   }
 
-  private static Stream<Arguments> loadClassPassTestCases() {
-    final List<JsonNode> testCases = JsonTestUtils.loadClassPathJson(TEST_CASES_FILE_PATH,
+  public static Stream<Arguments> loadTestCases(final String resourceFilePath) {
+    final List<JsonNode> testCases = JsonTestUtils.loadClassPathJson(resourceFilePath,
         new TypeReference<>() {
         });
 
