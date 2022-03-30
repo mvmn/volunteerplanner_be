95eeb13e1b3e16ba23e5d5ed2361350faed5191f
Mykola Makhin <mvmn.inbox@gmail.com>
2022-03-30 23:02:12 +0300

    Fixed broken subtask DTO - missing no args constructor

--- manager-api/src/main/java/com/volunteer/api/data/model/api/SubtaskDtoV1.java
+++ manager-api/src/main/java/com/volunteer/api/data/model/api/SubtaskDtoV1.java
@@ -6,10 +6,14 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.volunteer.api.data.model.SubtaskStatus;
 import java.math.BigDecimal;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @JsonPropertyOrder({"id", "taskId", "quantity", "status", "note", "transportRequired",
     "createdBy", "createdAt", "closedBy", "closedAt"})
