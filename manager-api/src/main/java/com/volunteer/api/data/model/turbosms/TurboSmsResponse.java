af3d74f142156a62e95061cfb789f714898a09c8
Mykola Makhin <mvmn.inbox@gmail.com>
2022-04-10 02:15:10 +0300

    Draft TurboSMS integration implementation - mock feign client to return success result by default.

--- manager-api/src/main/java/com/volunteer/api/data/model/turbosms/TurboSmsResponse.java
+++ manager-api/src/main/java/com/volunteer/api/data/model/turbosms/TurboSmsResponse.java
@@ -1,9 +1,15 @@
 package com.volunteer.api.data.model.turbosms;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TurboSmsResponse {
 
   @JsonProperty("response_code")
--- manager-api/src/test/java/com/volunteer/api/config/MockFeignConfig.java
+++ manager-api/src/test/java/com/volunteer/api/config/MockFeignConfig.java
@@ -3,6 +3,7 @@ package com.volunteer.api.config;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.volunteer.api.data.model.turbosms.TurboSmsResponse;
 import com.volunteer.api.feign.TurboSmsFeignClient;
 
 @Configuration
@@ -10,6 +11,9 @@ public class MockFeignConfig {
 
   @Bean
   public TurboSmsFeignClient turboSmsMock() {
-    return Mockito.mock(TurboSmsFeignClient.class);
+    TurboSmsFeignClient mock = Mockito.mock(TurboSmsFeignClient.class);
+    Mockito.when(mock.send(Mockito.any(), Mockito.any()))
+        .thenReturn(TurboSmsResponse.builder().responseCode(0).build());
+    return mock;
   }
 }
