b700ff1c4ca4273e463ff9808dfb3d4f3e76aad7
Mykola Makhin <mvmn.inbox@gmail.com>
2022-04-13 21:21:49 +0300

    Ensure authorize button on SwaggerUI

--- manager-api/src/main/java/com/volunteer/api/config/SecurityConfiguration.java
+++ manager-api/src/main/java/com/volunteer/api/config/SecurityConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -87,7 +86,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-  @Profile("local")
   public OpenAPI customOpenAPI() {
     final String securitySchemeName = "bearerAuth";
     return new OpenAPI().addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
