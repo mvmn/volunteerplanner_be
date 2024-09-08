249bd563abe5bad6db68e7f6e0300c07df03c370
Mykola Makhin <mvmn.inbox@gmail.com>
2024-09-08 18:03:50 +0300

    Allow having embedded UI, fixed some compilation/test issues.

--- manager-api/src/test/java/com/volunteer/api/config/CacheTestConfigurationRedis.java
+++ manager-api/src/test/java/com/volunteer/api/config/CacheTestConfigurationRedis.java
@@ -1,11 +1,14 @@
 package com.volunteer.api.config;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import redis.embedded.RedisServer;
+import redis.embedded.core.RedisServerBuilder;
 
 @ConditionalOnProperty(name = "cache.type", havingValue = "redis")
 @TestConfiguration
@@ -13,8 +16,8 @@ public class CacheTestConfigurationRedis {
 
   private final RedisServer redisServer;
 
-  public CacheTestConfigurationRedis(final RedisProperties redisProperties) {
-    this.redisServer = RedisServer.builder()
+  public CacheTestConfigurationRedis(final RedisProperties redisProperties) throws IOException {
+    this.redisServer = new RedisServerBuilder()
         .port(redisProperties.getPort())
         .setting("maxmemory 128M")
         .build();
@@ -22,13 +25,12 @@ public class CacheTestConfigurationRedis {
   }
 
   @PostConstruct
-  public void setup() {
+  public void setup() throws IOException {
     redisServer.start();
   }
 
   @PreDestroy
-  public void tearDown() {
+  public void tearDown() throws IOException {
     redisServer.stop();
   }
-
 }
